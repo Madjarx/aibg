ab8d41238ea067a050a84b2241d3cb8f1d812dc5
Luka Radisavljevic <lukaradisavljevic30@gmail.com>
2023-12-09 23:00:26 +0100

    feat: sanitizer initiated

--- src/Connector/Sanitizer.js
+++ src/Connector/Sanitizer.js
@@ -1,10 +1,63 @@
 /**
- * This class is meant to sanitize and pick out data that we specifically need
+ * This class is meant to sanitize and pick out data that we specifically need.
+ * Meant to be used to help other team members get their hands on data more easily
  * 
  * Refer to ~./dump/gameState.json for more information about example data
  * 
+ * NOTE - since its a hackathon, go with the assumption that everything exists!!! no null safety
+ * 
+ * 
+ * ######################
+ * 
+ * 
  * Class Sanitizer
+ * 
+ * 
  */
 module.exports = class Sanitizer {
-    //
+     
+    /**
+     * Class properties
+     */
+    _gameState;
+
+
+    /**
+     * Constructor
+     * 
+     * @param {Object} gameState 
+     */
+    constructor(gameState) { 
+        this._gameState = gameState;
+    };
+
+
+    /**
+     * Returns the initial coordinates based on the given id
+     */
+    static getInitialCoordinates() {
+        //
+    };
+
+    static getRocks() {
+        //
+    };
+
+    static getChest() {
+        //
+    };
+
+    static getTrees() {
+        //
+    };
+
+    static getField() {
+        //
+    };
+
+    static getNeighbourFields() {
+        //
+    };
+
+    
 };
---
+++ src/types/types.js
@@ -0,0 +1,7 @@
+/**
+ * This is a hacky way to describe types in nodeJS
+ */
+
+/**
+ * @typedef gameState
+ */
